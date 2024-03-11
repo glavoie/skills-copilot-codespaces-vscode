70572c8c531b39a149e5cf6ae95055cf6e3f3f8c
Gabriel Lavoie <glavoie@gmail.com>
2024-03-11 17:19:09 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,11 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+function calculateNumbers2(var1, var2) {
+    return var1 - var2;
+}
+
+function divideNumbers(var1, var2) {
+    return var1 / var2;
+}
